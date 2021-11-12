ab9768a11292129ecdb38f0485d003886d3a57ed
Johanu GANDONOU <johanugandiniu@gmail.com>
2021-11-12 19:31:56 +0100

    init

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,7 @@ Bundler.require(*Rails.groups)
 
 module Shop
   class Application < Rails::Application
+    config.assets.initialize_on_precompile = false
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
@@ -44,5 +45,6 @@ module Shop
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    
   end
 end
